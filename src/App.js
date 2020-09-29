184283113670122d0841cc57e86d526610479d8c
Jaynold Barreto <jbarreto@teknorix.in>
2020-09-29 14:26:45 +0530

    init react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router";
+import Home from "./components/Home";
+import MyForm from "./components/MyForm";
+import TableView from "./components/TableView";
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
